b661239d0c705432f08a57be65d06a1c78b564a3
Arslan Khabutdinov <khabutdinov.aa@phystech.edu>
2022-09-09 10:34:01 +0300

    add coredump and asan

---
+++ sem01-intro/bad.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int get(int *a, int i) {
+    return a[i];
+}
+
+int main() {
+    int a[10];
+    printf("%d\n", get(a, 2));
+    printf("%d\n", get(a, 27));
+    printf("%d\n", get(a, 2007));
+    printf("%d\n", get(a, 4007));
+}

87fe5a6702d9b2ad35a737be5547413034b86f7e
Arslan Khabutdinov <khabutdinov.aa@phystech.edu>
2022-09-11 20:11:45 +0300

    make info fixes

--- sem01-intro/bad.c
+++ sem01-intro/bad.c
@@ -7,7 +7,7 @@ int get(int *a, int i) {
 int main() {
     int a[10];
     printf("%d\n", get(a, 2));
-    printf("%d\n", get(a, 27));
-    printf("%d\n", get(a, 2007));
+    printf("%d\n", a[27]);
+    printf("%d\n", a[2007]);
     printf("%d\n", get(a, 4007));
 }

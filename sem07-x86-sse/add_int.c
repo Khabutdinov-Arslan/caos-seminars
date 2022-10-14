5b0e2fb8c731bf43a2ab4a16efa830b976133b35
Arslan Khabutdinov <khabutdinov.aa@phystech.edu>
2022-10-14 14:05:43 +0300

    still no reading

--- sem07-x86-sse/add_int.c
+++ sem07-x86-sse/add_int.c
@@ -13,8 +13,8 @@ int main(){
 		b[i] = 2 * (i + 1);
 	}
 	for (int i = 0; i < n; i+= 8) {
-        __m256 r1 = _mm256_load_ps((const float*)&a[i]);
-        __m256 r2 = _mm256_load_ps((const float*)&b[i]);
+        __m256 r1 = _mm256_load_ps(a + i);
+        __m256 r2 = _mm256_load_ps(b + i);
         __m256 r3 = _mm256_add_ps(r1, r2);
         _mm256_store_ps(&c[i], r3);
     }

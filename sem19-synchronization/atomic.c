4e2092e020e8c41f84ac8b1ec945dbd5d3b43881
Arslan Khabutdinov <khabutdinov.aa@phystech.edu>
2022-10-15 08:05:39 +0300

    init sem19 finish pls

---
+++ sem19-synchronization/atomic.c
@@ -0,0 +1,36 @@
+// solution by Alexey Zertsalov
+
+#include <stdio.h>
+#include <pthread.h>
+#include <stdatomic.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <stdlib.h>
+
+typedef struct {
+    pthread_t thread;
+    int* counter;
+} thread_data;
+
+static void* thread_func(void* arg) {
+    thread_data* data_ptr = (thread_data*)arg;
+    for (int i = 0; i < 100000000; i++) {
+        atomic_fetch_add(data_ptr->counter, 1);
+    }
+    return NULL;
+}
+
+int main() {
+    const int THREADS_COUNT = 10;
+    thread_data threads[THREADS_COUNT];
+    int shared_counter = 0;
+    for (int i = 0; i < THREADS_COUNT; i++) {
+        threads[i].counter = &shared_counter;
+        pthread_create(&threads[i].thread, NULL, thread_func,  (void*)&threads[i]);
+    }
+    for (int i = 0; i < THREADS_COUNT; i++) {
+        pthread_join(threads[i].thread, NULL);
+    }
+    printf("%d\n", shared_counter);
+    return 0;
+}
---
+++ sem19-synchronization/mutex.c
@@ -0,0 +1,44 @@
+// solution by Alexey Zertsalov
+
+#include <stdio.h>
+#include <pthread.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <stdlib.h>
+
+typedef struct {
+    pthread_t thread;
+    int* counter;
+    pthread_mutex_t* syncer;
+} thread_data;
+
+static void* thread_func(void* arg) {
+    thread_data* data_ptr = (thread_data*)arg;
+    int local_counter = 0;
+    for (int i = 0; i < 100000000; i++) {
+        local_counter++;
+    }
+    pthread_mutex_lock(data_ptr->syncer);
+    (*(data_ptr->counter)) += local_counter;
+    pthread_mutex_unlock(data_ptr->syncer);
+    return NULL;
+}
+
+int main() {
+    pthread_mutex_t syncer;
+    pthread_mutex_init(&syncer, NULL);
+
+    const int THREADS_COUNT = 10;
+    thread_data threads[THREADS_COUNT];
+    int shared_counter = 0;
+    for (int i = 0; i < THREADS_COUNT; i++) {
+        threads[i].counter = &shared_counter;
+        threads[i].syncer = &syncer;
+        pthread_create(&threads[i].thread, NULL, thread_func,  (void*)&threads[i]);
+    }
+    for (int i = 0; i < THREADS_COUNT; i++) {
+        pthread_join(threads[i].thread, NULL);
+    }
+    printf("%d\n", shared_counter);
+    return 0;
+}

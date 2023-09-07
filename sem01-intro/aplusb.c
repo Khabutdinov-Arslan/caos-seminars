77b8a10bac33aacf73cdccfb00c68673dcfc131f
Arslan Khabutdinov <khabutdinov.aa@phystech.edu>
2023-09-07 17:22:38 +0300

    init sem01

---
+++ sem01-intro/aplusb.c
@@ -0,0 +1,11 @@
+#define ONE 1
+#define TWO 2
+
+// simple program
+
+int main() {
+    int a = ONE;
+    int b = TWO;
+    int c = a + b;
+    return 0;
+}
---
+++ sem01-intro/aplusb_preprocessed.c
@@ -0,0 +1,17 @@
+# 0 "aplusb.c"
+# 0 "<built-in>"
+# 0 "<command-line>"
+# 1 "/usr/include/stdc-predef.h" 1 3 4
+# 0 "<command-line>" 2
+# 1 "aplusb.c"
+
+
+
+
+
+int main() {
+    int a = 1;
+    int b = 2;
+    int c = a + b;
+    return 0;
+}

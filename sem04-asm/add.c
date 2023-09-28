b5986a6c811f538161022b005c0a6fb1c0feec2a
Arslan Khabutdinov <khabutdinov.aa@phystech.edu>
2023-09-28 12:54:28 +0300

    sem04-asm fixes

--- sem04-asm/add.c
+++ sem04-asm/add.c
@@ -1,3 +1,5 @@
+#include <limits.h>
+#include <stdalign.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -6,9 +8,11 @@ very_important_function(int N, const float *A, const float *B, float *R);
 
 int main(){
 	const size_t n = 8;
-	float *a = aligned_alloc(32, 32 * n);
-	float *b = aligned_alloc(32, 32 * n);
-	float *c = aligned_alloc(32, 32 * n);
+	const size_t alig = alignof(float) * CHAR_BIT;
+	const size_t sz = alig * n;
+	float *a = aligned_alloc(alig , sz);
+	float *b = aligned_alloc(alig, sz);
+	float *c = aligned_alloc(alig, sz);
 	for (int i = 0; i < n; i++){
 		a[i] = i + 1;
 		b[i] = 2 * (i + 1);
